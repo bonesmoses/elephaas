b93da79528ab8ba24f5d92f33603f7988b19e209
Shaun Thomas <sthomas@peak6.com>
2015-12-23 10:17:57 -0600

    * Removed HAAS stuff from 1.9 migration branch.

--- haas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class HAASApp(AppConfig):
-    name = 'haas'
-    verbose_name = 'Elephant Herd as a Service'
--- haas/sql/environment.sql
+++
@@ -1,5 +0,0 @@
-CREATE TRIGGER t_env_audit_stamp_b_iu
-       BEFORE INSERT OR UPDATE
-    ON ele_environment
-   FOR EACH ROW
-       EXECUTE PROCEDURE sp_audit_stamps();
