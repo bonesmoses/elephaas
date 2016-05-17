f749d1500f14d1bcf4103a1f66415130a9159281
Shaun Thomas <sthomas@peak6.com>
2016-05-17 16:30:52 -0500

    * Removed deprecated Django < 1.8 custom SQL files.

--- haas/sql/herd.sql
+++
@@ -1,5 +0,0 @@
-CREATE TRIGGER t_herd_audit_stamp_b_iu
-       BEFORE INSERT OR UPDATE
-    ON ele_herd
-   FOR EACH ROW
-       EXECUTE PROCEDURE sp_audit_stamps();
--- haas/sql/instance.sql
+++
@@ -1,5 +0,0 @@
-CREATE TRIGGER t_instance_audit_stamp_b_iu
-       BEFORE INSERT OR UPDATE
-    ON ele_instance
-   FOR EACH ROW
-       EXECUTE PROCEDURE sp_audit_stamps();
