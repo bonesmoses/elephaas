0edff18c626efe4b613cfa74b74261bc5ea88a92
Shaun Thomas <sthomas@peak6.com>
2017-04-12 15:48:21 -0500

    * Removed accidental inclusion of debian files.

--- debian/elephaas/opt/elephaas/elephaas/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-def global_settings(request):
-    # return any necessary values
-    return {
-        'PUBLIC_KEY': settings.PUBLIC_KEY,
-    }
--- debian/elephaas/opt/elephaas/haas/admin/__init__.py
+++
@@ -1,5 +0,0 @@
-from haas.admin.dr import *
-from haas.admin.environment import *
-from haas.admin.herd import *
-from haas.admin.instance import *
-from haas.admin.server import *
